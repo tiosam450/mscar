233debe74ad95a581f1534a8ffba2ddb819a6c9d
Samuel Nogueira <tiosam450@hotmail.com>
2024-09-12 20:35:09 -0400

    Desk Casa

--- src/contexAPI.tsx
+++ src/contexAPI.tsx
@@ -1,15 +1,61 @@
-import { createContext, ReactNode } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { auth } from "./services/conexaoFireBase";
 
-interface childrenProps{
+interface ChildrenProps {
     children: ReactNode
 }
 
-const conteudoAPI = createContext({})
-
-export function apiProvider ({children}:childrenProps){
-    <conteudoAPI.Provider value={{
-
-    }}>
-        {children}
-    </conteudoAPI.Provider>
+interface UserProps{
+    uid: string
+    nome: string | null
+    email: string | null
 }
+
+type DadosApi = {
+    logado: boolean;
+    loadingAuth: boolean;
+}
+
+
+const conteudoAPI = createContext({} as DadosApi)
+
+export function ApiProvider({ children }: ChildrenProps) {
+
+    const [usuario, setUsuario] = useState<UserProps | null>()
+    const [loadingAuth, setLoadingAuth] = useState(true)
+
+    useEffect(()=>{
+        const checkUsuario = onAuthStateChanged(auth, (user)=>{
+            if (user){
+            setUsuario({
+                uid: user.uid,
+                nome: user?.displayName,
+                email: user?.email,
+            })
+            setLoadingAuth(false)
+            }else{
+                setUsuario(null)
+                setLoadingAuth(false)
+            }
+        })
+
+        console.log(usuario)
+
+        return () => checkUsuario()
+        
+
+    },[])
+
+    return (
+        <conteudoAPI.Provider value={{
+            logado: !!usuario,
+            loadingAuth,
+
+        }}>
+            {children}
+        </conteudoAPI.Provider>
+    )
+}
+
+export default conteudoAPI
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import rotas from './rotas/rotas'
 import { Toaster } from 'react-hot-toast'
+import { ApiProvider } from './contexAPI'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Toaster position="top-center" reverseOrder={false}/>
-     <RouterProvider router={rotas} />
+    <Toaster position="top-center" reverseOrder={false} />
+    <ApiProvider>
+      <RouterProvider router={rotas} />
+    </ApiProvider>
   </StrictMode>,
 )
